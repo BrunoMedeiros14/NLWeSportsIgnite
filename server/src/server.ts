49bdcd54dd7608aae3c0964c991f0e044380274c
Bruno <brunodemedeiros14@gmail.com>
2022-09-16 21:42:15 -0300

    Adicionado rota para inclusão e remoção de jogos.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,52 +1,32 @@
-import cors from 'cors'
+import cors from "cors";
 import express from "express";
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 import { convertHourStringToMinutes } from "./utils/convert-hour-string-to-minutes";
 import { convertMinutestoHourString } from "./utils/convert-minutes-to-hour-string";
 
 const port = 3333;
 const app = express();
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/games', async (request, response) => {
+app.get("/games", async (request, response) => {
   const games = await prisma.game.findMany({
     include: {
       _count: {
         select: {
-          ads: true
-        }
-      }
-    }
-  })
-  return response.json(games)
-})
-
-app.post('/games/:id/ads', async (request, response) => {
-  const gameId = request.params.id
-  const body: any = request.body
-
-  const ad = await prisma.ad.create({
-    data: {
-      gameId,
-      name: body.name,
-      yearsPlaying: body.yearsPlaying,
-      discord: body.discord,
-      weekDays: body.weekDays.join(','),
-      hourStart: convertHourStringToMinutes(body.hourStart),
-      hourEnd: convertHourStringToMinutes(body.hourEnd),
-      useVoiceChannel: body.useVoiceChannel,
-    }
-  })
-
-  return response.status(201).json(ad)
-})
+          ads: true,
+        },
+      },
+    },
+  });
+  return response.json(games);
+});
 
 app.get("/games/:id/ads", async (request, response) => {
-  const gameId = request.params.id
+  const gameId = request.params.id;
   const ads = await prisma.ad.findMany({
     select: {
       id: true,
@@ -58,36 +38,79 @@ app.get("/games/:id/ads", async (request, response) => {
       hourEnd: true,
     },
     orderBy: {
-      CreatedAt: 'desc',
+      CreatedAt: "desc",
     },
     where: {
       gameId,
-    }
-  })
+    },
+  });
   return response.json(
-    ads.map(ad => {
+    ads.map((ad) => {
       return {
         ...ad,
-        weekDays: ad.weekDays.split(','),
+        weekDays: ad.weekDays.split(","),
         hourStart: convertMinutestoHourString(ad.hourStart),
         hourEnd: convertMinutestoHourString(ad.hourEnd),
-      }
-    }))
+      };
+    })
+  );
 });
 
 app.get("/games/:id/discord", async (request, response) => {
-  const adId = request.params.id
+  const adId = request.params.id;
   const ad = await prisma.ad.findUniqueOrThrow({
     select: {
       discord: true,
     },
     where: {
       id: adId,
-    }
-  })
+    },
+  });
   return response.json({
     discord: ad.discord,
-  })
+  });
+});
+
+app.post("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
+  const body: any = request.body;
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(","),
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    },
+  });
+
+  return response.status(201).json(ad);
+});
+
+app.post("/games", async (request, response) => {
+  const { title, bannerUrl } = request.body;
+
+  const add = await prisma.game.create({
+    data: { title, bannerUrl },
+  });
+
+  return response.status(201).json(add);
+});
+
+app.delete("/games/:id", async (request, response) => {
+  const id = request.params.id;
+
+  const removedGame = await prisma.game.delete({
+    where: {
+      id,
+    },
+  });
+
+  return response.status(201).json({ status: "Game deleted", ...removedGame });
 });
 
 app.listen(port, () => {
